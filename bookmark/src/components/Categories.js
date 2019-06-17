4cbad0107ceddc5625e90ae2b9a4669d4d79b9fc
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-06-16 21:04:55 -0700

    cards material ui working

--- bookmark/src/components/Categories.js
+++ bookmark/src/components/Categories.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import '../styles.css'
+import SimpleCard from './SimpleCard'
 
 class Categories extends React.Component {
   state = {
@@ -13,9 +15,24 @@ class Categories extends React.Component {
   render() {
     return (
       <>
-        <h4>Which companies do you pay?</h4>
+      <div className = 'categoryContainer'>
+         <h4>Which companies do you pay?</h4>
+         <Button>Don't see one here? Add your own!</Button>
 
-        <Button>Don't see one here? Add your own!</Button>
+        <div className = 'catCardsContainer'>
+        <div  className = 'categoryCards'> 
+        <SimpleCard />
+        </div>
+        <div className = 'categoryCards'> 
+        <SimpleCard />
+        </div>
+
+        </div>
+
+      </div>
+        
+
+
       </>
     );
   }
