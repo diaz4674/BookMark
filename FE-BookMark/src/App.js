813099a9f1addb04c7070a5ecb07af20874e3940
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-16 09:58:12 -0700

    removed unsued component

--- FE-BookMark/src/App.js
+++ FE-BookMark/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard.js";
-import Categories from "./components/Onboarding/Categories.js";
 import Login from "./components/Login.js";
 import SignUp from "./components/Onboarding/SignUp";
 import FinancialCard from "./components/Onboarding/FinancialCard";
 import ShoppingCard from "./components/Onboarding/ShoppingCard";
 import PersonalCard from "./components/Onboarding/PersonalCard";
 import Welcome from "./components/Onboarding/welcome";
-import onboardNav from "./components/Navbars/OnboardNav";
 import FinancialDashboard from "./components/FinancialDashboard";
+
 function App() {
   return (
     <div className="App">
@@ -21,12 +20,7 @@ function App() {
           <Route path="/shoppingSelect" component={ShoppingCard} />
           <Route path="/personalSelect" component={PersonalCard} />
           <Route path="/financialCard" component={FinancialDashboard} />
-          <Route
-            path="/categories"
-            render={props => <Categories {...props} />}
-          />
           <Route exact path="/" component={Login} />
-
           <Route path="/dashboard" component={Dashboard} />
         </Switch>
       </Router>
