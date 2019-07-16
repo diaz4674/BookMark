909817b4a0b4fa1de2c65a9d17e3eccf57538f31
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-16 13:47:44 -0700

    added comments

--- FE-BookMark/src/components/Onboarding/Categories.js
+++ FE-BookMark/src/components/Onboarding/Categories.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Button from "@material-ui/core/Button";
-import FinancialCard from "./FinancialCard";
-import ShoppingCard from "./ShoppingCard";
-import PersonalCard from "./PersonalCard";
+//Component that contains the images that display in the Welcome Component
+
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import CardActions from "@material-ui/core/CardActions";
 import CardMedia from "@material-ui/core/CardMedia";
-import { Link } from "react-router-dom";
 import "../../styles.css";
 
 const useStyles = makeStyles(theme => ({
+  //Component CSS
   cardsContainer: {
     display: "flex",
     justifyContent: "space-around",
