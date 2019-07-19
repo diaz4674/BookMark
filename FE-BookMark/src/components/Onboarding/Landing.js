0303e6f6dde76f6a23a759572e7f69811678aad7
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-19 10:57:41 -0700

    added section to landing page

--- FE-BookMark/src/components/Onboarding/Landing.js
+++ FE-BookMark/src/components/Onboarding/Landing.js
@@ -2,35 +2,53 @@ import React from "react";
 import OnboardNav from "../Navbars/OnboardNav";
 import "../../styles.css";
 import { Link } from "react-router-dom";
-import bookmark from '../../images/bookmark.jpg'
+import bookmark from "../../images/bookmark.jpg";
 import { makeStyles } from "@material-ui/core/styles";
+import personalCard from "../../images/personal.PNG";
 
 const useStyles = makeStyles(theme => ({
   backgroundImg: {
-    width: "100%",
-    height: "100vh",
+    width: "100%"
   },
+
   landingContainer: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    overflow: "hidden",
+    overflow: "hidden"
   },
   landingText: {
-    top: "30%",
-    left: "15%",
-    borderRadius: "10px",
+    padding: "0px 50px",
     position: "absolute",
-    height: "450px",
-    padding: "25px",
-    margin: "-107px",
-    width: "450px",
-    fontFamily: "Roboto Slab, serif",
-    fontWeight: "bold",
-    backgroundColor: "white",
-    border: "1px solid black"
+    borderRadius: "50%",
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap"
+    // ["@media (max-width:1305px)"]: {
+    //   top: "10%",
+    //   left: "5%"
+    // }
+  },
+  secondSection: {
+    backgroundColor: "#faf9f7"
+  },
+  sectionExample: {
+    display: "flex",
+    justifyContent: "space-around",
+    margin: "25px",
+    ["@media (max-width:770px)"]: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    }
+  },
+  sectionText: {
+    padding: "15px"
+  },
+  personalImg: {
+    height: "25em"
   }
-}))
+}));
 
 const Landing = props => {
   const classes = useStyles();
@@ -38,27 +56,38 @@ const Landing = props => {
 
   return (
     <>
-      <OnboardNav />
       <div>
-        <div className= {classes.landingContainer}>
-          <img src={bookmark} alt="computer" className={classes.backgroundImg} />
+        <div className={classes.landingContainer}>
+          <img
+            src={bookmark}
+            alt="computer"
+            className={classes.backgroundImg}
+          />
 
-          <div className= {classes.landingText}>
-            <div >
-              <div >
-                  <h1> A place for all your favorite sites</h1>
-                  <div>
-                    <p>
-                      Store your sites, so you can save on google searches.
-                    </p>
-                  </div>
-                <div className="buttons-landing">
-                  <Link className="cover this" to="/login">
-                    Login
-                  </Link>
-                </div>
-              </div>
+          <div className={classes.landingText}>
+            <h1> A place for all your favorite sites</h1>
+            {/* <div>
+                <p>
+                  Store your sites, so you can save time from google searches.
+                </p>
+              </div> */}
+            <div className="buttons-landing">
+              <Link className="cover this" to="/login">
+                Login
+              </Link>
             </div>
+          </div>
+        </div>
+        <div className={classes.secondSection}>
+          <div className={classes.sectionExample}>
+            <div className={classes.sectionText}>
+              <h3>Bookmark</h3>
+              <p>
+                Save time from constant google searches, and have all your
+                favorite sites in one place.
+              </p>
+            </div>
+            <img src={personalCard} className={classes.personalImg} />
           </div>
         </div>
       </div>
